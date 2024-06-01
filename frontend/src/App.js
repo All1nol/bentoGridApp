e8502475f60559d0ed610c0e854006625ea6bec2
newacckim <newacckim@gmail.com>
2024-06-02 00:21:50 +0400

    asfasf

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ const App = () => {
                     <Sidebar />
                     <div className="bento-grid-container">
                         <AppContent />
+                        "FAsfs"
                     </div>
                 </div>
             </div>
